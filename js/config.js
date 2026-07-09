8ac873922f6be4fbd91411a49036c8a3abc965cc
comarquesaplicacao <comarquesaplicacao@gmail.com>
2026-07-09 07:52:49 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Cole aqui a URL do seu Web App do Apps Script após o deploy
 // (Implantar > Gerenciar implantações > copiar a URL do "Web app")
-const APPS_SCRIPT_URL = 'COLE_AQUI_A_URL_DO_APPS_SCRIPT';
+const APPS_SCRIPT_URL = 'https://comarquesaplicacao.github.io/comarques-captacao-curriculos/';
